7a227ad9ab588bc2e1842a1e5e1489403c3c50a5
Woland A <sasha13ostapenko@gmail.com>
2019-10-24 17:23:27 +0300

    The project was created.

---
+++ Module3/Module3_1/Parser.cs
@@ -0,0 +1,21 @@
+﻿namespace Module3_1
+{
+	class Parser
+	{
+		private ConsoleReader reader;
+
+		public Parser()
+		{
+			reader = new ConsoleReader();
+		}
+
+		public int ParseToInt32(string str)
+		{
+			if (int.TryParse(str, out int number))
+			{
+				return number;
+			}
+			return ParseToInt32(reader.GetInput("Значение неверно, введите целое число: "));
+		}
+	}
+}
---
+++ Module3/Module3_1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Module3_1
+{
+	class Program
+	{
+		static void Main()
+		{
+			var reader = new ConsoleReader();
+			var parser = new Parser();
+
+			int firstNumber = parser.ParseToInt32(reader.GetInput("Введите первое число: "));
+			int secondNumber = parser.ParseToInt32(reader.GetInput("Введите второе число: "));
+
+			int result = Multiply(firstNumber, secondNumber);
+			Console.WriteLine($"Результат умножения: {result}");
+
+			Console.ReadKey();
+		}
+
+		static int Multiply(int firstNumber, int secondNumber)
+		{
+			if (firstNumber == 0 || secondNumber == 0)
+			{
+				return 0;
+			}
+
+			CheckSign(ref firstNumber, ref secondNumber);
+
+			int result = 0;
+
+			for (int index = 0; index < secondNumber; index++)
+			{
+				result += firstNumber;
+			}
+
+			return result;
+		}
+
+		static void CheckSign(ref int firstNumber, ref int secondNumber)
+		{
+			if (secondNumber < 0)
+			{
+				secondNumber -= secondNumber + secondNumber;
+				if (firstNumber < 0)
+				{
+					firstNumber -= firstNumber + firstNumber;
+				}
+				else
+				{
+					firstNumber -= firstNumber + firstNumber;
+				}
+			}
+			
+		}
+	}
+}
