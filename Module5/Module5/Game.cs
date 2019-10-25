e8ae4278bd63e9e3b241c478c0dd587222a705b3
Woland A <sasha13ostapenko@gmail.com>
2019-10-25 20:28:33 +0300

    Structure of the project was started.

---
+++ Module5/Module5/Game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module5
+{
+	class Game
+	{
+		private Cell[] field;
+
+		public Game()
+		{
+			field = new Cell[100];
+		}
+
+		public void Start()
+		{
+
+		}
+	}
+}
