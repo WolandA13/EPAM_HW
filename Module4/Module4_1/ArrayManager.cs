960726e312c387a69d324e0c3455fd9995fbf362
Woland A <sasha13ostapenko@gmail.com>
2019-10-29 20:41:29 +0300

    ArrayManager was created.

---
+++ Module4/Module4_1/ArrayManager.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module4_1
+{
+	class ArrayManager
+	{
+		public int[] CreateRandomArray(int length, int leftBound, int rightBound)
+		{
+
+		}
+
+		public int GetMaxElementFromArray(int[] array)
+		{
+			int max = array[0];
+			foreach (int element in array)
+			{
+				if (element > max)
+				{
+					max = element;
+				}
+			}
+			return max;
+		}
+
+		public int GetMinElementFromArray(int[] array)
+		{
+			var min = array[0];
+			foreach (var element in array)
+			{
+				if (element < min)
+				{
+					min = element;
+				}
+			}
+			return min;
+		}
+
+		public int GetSumOfArrayElements(int[] array)
+		{
+			var sum = 0;
+			foreach (var element in array)
+			{
+				sum += element;
+			}
+			return sum;
+		}
+
+		public int GetDifferBtwnMaxAndMinInArray(int[] array)
+		{
+			return GetMaxElementFromArray(array) - GetMinElementFromArray(array);
+		}
+
+		public void ChangeElementsInArray(int[] array)
+		{
+			var max = GetMaxElementFromArray(array);
+			var min = GetMinElementFromArray(array);
+
+			for (int index = 0; index < array.Length; index++)
+			{
+				array[index] = (array[index] % 2 == 0) ?
+					array[index] + max :
+					array[index] - min;
+			}
+		}
+	}
+}
--- Module4/Module4_1/Program.cs
+++ Module4/Module4_1/Program.cs
@@ -1,70 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Module4_1
+﻿namespace Module4_1
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
 
-		}
-
-		static int GetMaxElementFromArr(int[] array)
-		{
-			int max = array[0];
-			foreach (int element in array)
-			{
-				if (element > max)
-				{
-					max = element;
-				}
-			}
-			return max;
-		}
-
-		static int GetMinElementFromArr(int[] array)
-		{
-			var min = array[0];
-			foreach (var element in array)
-			{
-				if (element < min)
-				{
-					min = element;
-				}
-			}
-			return min;
-		}
-
-		static int GetSumOfArrElements(int[] array)
-		{
-			var sum = 0;
-			foreach (var element in array)
-			{
-				sum += element;
-			}
-			return sum;
-		}
-
-		static int GetDifferBtwnMaxAndMinInArr(int[] array)
-		{
-			return GetMaxElementFromArr(array) - GetMinElementFromArr(array);
-		}
-
-		static void ChaneElementsInArr(int[] array)
-		{
-			var max = GetMaxElementFromArr(array);
-			var min = GetMinElementFromArr(array);
-
-			for (int index = 0; index < array.Length; index++)
-			{
-				array[index] = (array[index] % 2 == 0) ?
-					array[index] + max :
-					array[index] - min;
-			}
 		}
 	}
 }
