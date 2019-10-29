6a50f0c4d84e719f8012168e8572eda1097d5f1c
Woland A <sasha13ostapenko@gmail.com>
2019-10-29 20:29:22 +0300

    Module4_1 was created.

--- Module4/Module4_1/Program.cs
+++ Module4/Module4_1/Program.cs
@@ -10,6 +10,61 @@ namespace Module4_1
 	{
 		static void Main(string[] args)
 		{
+
+		}
+
+		static int GetMaxElementFromArr(int[] array)
+		{
+			int max = array[0];
+			foreach (int element in array)
+			{
+				if (element > max)
+				{
+					max = element;
+				}
+			}
+			return max;
+		}
+
+		static int GetMinElementFromArr(int[] array)
+		{
+			var min = array[0];
+			foreach (var element in array)
+			{
+				if (element < min)
+				{
+					min = element;
+				}
+			}
+			return min;
+		}
+
+		static int GetSumOfArrElements(int[] array)
+		{
+			var sum = 0;
+			foreach (var element in array)
+			{
+				sum += element;
+			}
+			return sum;
+		}
+
+		static int GetDifferBtwnMaxAndMinInArr(int[] array)
+		{
+			return GetMaxElementFromArr(array) - GetMinElementFromArr(array);
+		}
+
+		static void ChaneElementsInArr(int[] array)
+		{
+			var max = GetMaxElementFromArr(array);
+			var min = GetMinElementFromArr(array);
+
+			for (int index = 0; index < array.Length; index++)
+			{
+				array[index] = (array[index] % 2 == 0) ?
+					array[index] + max :
+					array[index] - min;
+			}
 		}
 	}
 }
