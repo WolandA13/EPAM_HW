c19b4b7fc7accd68cff764a6b88bf8ccd14cb799
Woland A <sasha13ostapenko@gmail.com>
2019-10-29 20:34:56 +0300

    IOLib was created.

---
+++ Module4/IOLib/ConsoleReader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace IOLib
+{
+	class ConsoleReader
+	{
+		public string GetInput(string message)
+		{
+			Console.Write(message);
+			string input = Console.ReadLine();
+			return input;
+		}
+	}
+}
---
+++ Module4/IOLib/Parser.cs
@@ -0,0 +1,30 @@
+﻿namespace IOLib
+{
+	class Parser
+	{
+		private ConsoleReader reader;
+
+		public Parser()
+		{
+			reader = new ConsoleReader();
+		}
+
+		public int ParseToInt32(string str)
+		{
+			if (int.TryParse(str, out int number))
+			{
+				return number;
+			}
+			return ParseToInt32(reader.GetInput("Значение неверно, введите целое число: "));
+		}
+
+		public double ParseToDouble(string str)
+		{
+			if (double.TryParse(str.Replace('.', ','), out double number))
+			{
+				return number;
+			}
+			return ParseToDouble(reader.GetInput("Значение неверно, введите число: "));
+		}
+	}
+}
