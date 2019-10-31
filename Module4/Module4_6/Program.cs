beda9f28b5fb15a79e40fde2470d051d3079db8b
Woland A <sasha13ostapenko@gmail.com>
2019-10-31 17:44:26 +0300

    Module4_6 was complete.

---
+++ Module4/Module4_6/Program.cs
@@ -0,0 +1,34 @@
+﻿using Module4_1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module4_6
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int dimensionOfIncrease = 5;
+			var arrayManager = new ArrayManager();
+
+			int[] array = arrayManager.CreateRandomArray(10, -10, 10);
+			arrayManager.WriteArray("Начальный массив:", array);
+
+			IncreaseArrayBy(dimensionOfIncrease, array);
+			arrayManager.WriteArray($"Изменённый массив (элементы которого увеличены на {dimensionOfIncrease}):", array);
+
+			Console.ReadKey();
+		}
+
+		static void IncreaseArrayBy(int dimensionOfIncrease, int[] array)
+		{
+			for (int index = 0; index < array.Length; index++)
+			{
+				array[index] += dimensionOfIncrease;
+			}
+		}
+	}
+}
