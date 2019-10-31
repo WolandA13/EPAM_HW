a7345fa16019d15b7e3ad2f1999cfe651434c133
Woland A <sasha13ostapenko@gmail.com>
2019-10-31 19:12:18 +0300

    Module4_8 was completed.

--- Module4/Module4_8/Program.cs
+++ Module4/Module4_8/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using IOLib;
+using System;
 
 namespace Module4_8
 {
@@ -10,6 +7,72 @@ namespace Module4_8
 	{
 		static void Main(string[] args)
 		{
+			double leftBound = 0;
+			double rightBound = 0;
+
+			EnterBounds(ref leftBound, ref rightBound);
+			double accuracy = EnterAccuracy();
+			double x = Calculate(leftBound, rightBound, accuracy);
+
+			Console.WriteLine("x = " + x);
+
+			Console.ReadKey();
+		}
+
+		static void EnterBounds(ref double leftBound, ref double rightBound)
+		{
+			var parser = new Parser();
+			var reader = new ConsoleReader();
+
+			leftBound = parser.ParseToDouble(reader.GetInput("Введите левую границу: "));
+			rightBound = parser.ParseToDouble(reader.GetInput("Введите правую границу: "));
+
+			if (leftBound > rightBound)
+			{
+				double tmp = leftBound;
+				leftBound = rightBound;
+				rightBound = tmp;
+			}
+
+			if (Func(leftBound) * Func(rightBound) >= 0)
+			{
+				Console.WriteLine("Данные не подходят.");
+				EnterBounds(ref leftBound, ref rightBound);
+			}
+		}
+
+		static double EnterAccuracy()
+		{
+			var parser = new Parser();
+			var reader = new ConsoleReader();
+
+			return parser.ParseToDouble(reader.GetInput("Введите точность: "));
+		}
+
+		static double Calculate(double leftBound, double rightBound, double accuracy)
+		{
+			double x;
+
+			x = (leftBound + rightBound) / 2;
+			if (Func(leftBound) * Func(x) < 0)
+			{
+				rightBound = x;
+			}
+			else if (Func(x) * Func(rightBound) < 0)
+			{
+				leftBound = x;
+			}
+
+			if (Math.Abs(rightBound - leftBound) <= 2 * accuracy)
+			{
+				return (leftBound + rightBound) / 2;
+			}
+			return Calculate(leftBound, rightBound, accuracy);
+		}
+
+		static double Func(double x)
+		{
+			return 5 * x - 10;
 		}
 	}
 }
